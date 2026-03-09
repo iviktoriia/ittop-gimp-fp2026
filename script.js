6bee4ba3469aab5768ed73d47b97dfa39f90603a
local user kb <testing@kb.com>
2026-03-09 11:24:35 +0300

    change filenames from ru to eng

--- script.js
+++ script.js
@@ -15,179 +15,182 @@ const data = {
   zombie: [
     {
       type: "npc",
-      src: "img/zombie/дима-в/нпс.png",
+      src: "img/zombie/dima-v/npc.png",
       author: "Дима В"
     },
     {
       type: "npc",
-      src: "img/zombie/вова/добрый-нпс.png",
+      src: "img/zombie/vova/dobriy-npc.png",
       author: "Вова"
     },
     {
       type: "npc",
-      src: "img/zombie/витя/нпс1.png",
+      src: "img/zombie/vitya/npc1.png",
       author: "Витя"
     },
     {
       type: "npc",
-      src: "img/zombie/витя/нпс2.png",
+      src: "img/zombie/vitya/npc2.png",
       author: "Витя"
     },
     {
       type: "background",
-      src: "img/zombie/дима-в/фон.png",
+      src: "img/zombie/dima-v/fon.png",
       author: "Дима В"
     },
     {
       type: "background",
-      src: "img/zombie/вова/фон.png",
+      src: "img/zombie/vova/fon.png",
       author: "Вова"
     },
     {
       type: "background",
-      src: "img/zombie/марк/фон.png",
+      src: "img/zombie/mark/fon.png",
       author: "Марк"
     },
     {
       type: "enemy",
-      src: "img/zombie/вова/злой-нпс.png",
+      src: "img/zombie/vova/zloy-npc.png",
       author: "Вова"
     },
     {
       type: "enemy",
-      src: "img/zombie/дима-д/ассет1.png",
+      src: "img/zombie/dima-d/asset1.png",
       author: "Дима Д"
     },
     {
       type: "enemy",
-      src: "img/zombie/дима-д/ассет2.png",
+      src: "img/zombie/dima-d/asset2.png",
       author: "Дима Д"
     },
     {
       type: "boss",
-      src: "img/zombie/марк/босс.png",
+      src: "img/zombie/mark/boss.png",
       author: "Марк"
     },
     {
       type: "boss",
-      src: "img/zombie/дима-д/босс.png",
+      src: "img/zombie/dima-d/boss.png",
       author: "Дима Д"
     },
     {
       type: "ui",
-      src: "img/zombie/вова/сердце.gif",
+      src: "img/zombie/vova/serdce.gif",
       author: "Вова"
     },
     {
       type: "ui",
-      src: "img/zombie/вова/меню.gif",
+      src: "img/zombie/vova/menu.gif",
       author: "Вова"
     },
     {
       type: "ui",
-      src: "img/zombie/вова/зомби-рука.gif",
+      src: "img/zombie/vova/zombi-ruka.gif",
       author: "Вова"
     },
     {
       type: "ui",
-      src: "img/zombie/матвей/экран-победы.png",
+      src: "img/zombie/matvey/ekran-pobedy.png",
       author: "Матвей"
     },
     {
       type: "ui",
-      src: "img/zombie/матвей/экран-поражения.png",
+      src: "img/zombie/matvey/ekran-porazheniya.png",
       author: "Матвей"
     },
     {
       type: "ui",
-      src: "img/zombie/дима-д/меню.png",
+      src: "img/zombie/dima-d/menu.png",
       author: "Дима Д"
     },
-    { type: "hero",
-      src: "img/zombie/матвей/главный-герой.png",
+    { 
+      type: "hero",
+      src: "img/zombie/matvey/glavniy-geroy.png",
       author: "Матвей"
     },
-    { type: "hero",
-      src: "img/zombie/матвей/гг-ассет1.png",
+    { 
+      type: "hero",
+      src: "img/zombie/matvey/gg-asset1.png",
       author: "Матвей"
     },
-    { type: "hero",
-      src: "img/zombie/матвей/гг-ассет2.png",
-      author: "Матвей"
-    },
-    {
-      type: "item",
-      src: "img/zombie/матвей/оружие1.png",
-      author: "Матвей"
-    },
-    {
-      type: "item",
-      src: "img/zombie/матвей/оружие2.png",
+    { 
+      type: "hero",
+      src: "img/zombie/matvey/gg-asset2.png",
       author: "Матвей"
     },
     {
       type: "item",
-      src: "img/zombie/дима-д/оружие.png",
+      src: "img/zombie/matvey/oruzhie1.png",
+      author: "Матвей"
+    },
+    {
+      type: "item",
+      src: "img/zombie/matvey/oruzhie2.png",
+      author: "Матвей"
+    },
+    {
+      type: "item",
+      src: "img/zombie/dima-d/oruzhie.png",
       author: "Дима Д"
     },
     {
       type: "item",
-      src: "img/zombie/дима-д/деньги.png",
+      src: "img/zombie/dima-d/dengi.png",
       author: "Дима Д"
     },
     {
       type: "item",
-      src: "img/zombie/витя/оружие.jpg",
+      src: "img/zombie/vitya/oruzhie.jpg",
       author: "Витя"
     },
     {
       type: "tiles",
-      src: "img/zombie/дима-д/тайл1.png",
+      src: "img/zombie/dima-d/tile1.png",
       author: "Дима Д"
     },
     {
       type: "tiles",
-      src: "img/zombie/дима-д/тайл2.png",
+      src: "img/zombie/dima-d/tile2.png",
       author: "Дима Д"
     },
     {
       type: "tiles",
-      src: "img/zombie/дима-д/тайл3.png",
+      src: "img/zombie/dima-d/tile3.png",
       author: "Дима Д"
     },
     {
       type: "tiles",
-      src: "img/zombie/дима-д/тайл4.png",
+      src: "img/zombie/dima-d/tile4.png",
       author: "Дима Д"
     },
     {
       type: "tiles",
-      src: "img/zombie/дима-д/тайл5.png",
+      src: "img/zombie/dima-d/tile5.png",
       author: "Дима Д"
     },
     {
       type: "tiles",
-      src: "img/zombie/дима-д/тайл6.png",
+      src: "img/zombie/dima-d/tile6.png",
       author: "Дима Д"
     },
     {
       type: "tiles",
-      src: "img/zombie/матвей/тайл1.png",
+      src: "img/zombie/matvey/tile1.png",
       author: "Матвей"
     },
     {
       type: "tiles",
-      src: "img/zombie/матвей/тайл2.png",
+      src: "img/zombie/matvey/tile2.png",
       author: "Матвей"
     },
     {
       type: "tiles",
-      src: "img/zombie/матвей/тайл3.png",
+      src: "img/zombie/matvey/tile3.png",
       author: "Матвей"
     },
     {
       type: "tiles",
-      src: "img/zombie/матвей/тайл4.png",
+      src: "img/zombie/matvey/tile4.png",
       author: "Матвей"
     },
   ],
@@ -195,227 +198,227 @@ const data = {
   horror: [
     {
       type: "npc",
-      src: "img/horror/настя/добрый-нпс.png",
+      src: "img/horror/nastya/dobriy-npc.png",
       author: "Настя"
     },
     {
       type: "boss",
-      src: "img/horror/егор/босс.png",
+      src: "img/horror/egor/boss.png",
       author: "Егор"
     },
     {
       type: "boss",
-      src: "img/horror/егор/помошник босса.png",
+      src: "img/horror/egor/pomoshchnik-bossa.png",
       author: "Егор"
     },
     {
       type: "boss",
-      src: "img/horror/юра/босс1.png",
+      src: "img/horror/yura/boss1.png",
       author: "Юра"
     },
     {
       type: "npc",
-      src: "img/horror/юра/босс2.png",
+      src: "img/horror/yura/boss2.png",
       author: "Юра"
     },
     {
       type: "tiles",
-      src: "img/horror/егор/тайл1.png",
+      src: "img/horror/egor/tile1.png",
       author: "Егор"
     },
     {
       type: "tiles",
-      src: "img/horror/егор/тайл2.png",
+      src: "img/horror/egor/tile2.png",
       author: "Егор"
     },
     {
       type: "tiles",
-      src: "img/horror/егор/тайл3.png",
+      src: "img/horror/egor/tile3.png",
       author: "Егор"
     },
     {
       type: "tiles",
-      src: "img/horror/юра/тайл1.png",
+      src: "img/horror/yura/tile1.png",
       author: "Юра"
     },
     {
       type: "tiles",
-      src: "img/horror/юра/тайл2.png",
+      src: "img/horror/yura/tile2.png",
       author: "Юра"
     },
     {
       type: "tiles",
-      src: "img/horror/юра/тайл3.png",
+      src: "img/horror/yura/tile3.png",
       author: "Юра"
     },
     {
       type: "tiles",
-      src: "img/horror/юра/тайл4.png",
+      src: "img/horror/yura/tile4.png",
       author: "Юра"
     },
     {
       type: "tiles",
-      src: "img/horror/юра/тайл5.png",
+      src: "img/horror/yura/tile5.png",
       author: "Юра"
     },
     {
       type: "tiles",
-      src: "img/horror/юра/тайл6.png",
+      src: "img/horror/yura/tile6.png",
       author: "Юра"
     },
     {
       type: "tiles",
-      src: "img/horror/юра/тайл7.png",
+      src: "img/horror/yura/tile7.png",
       author: "Юра"
     },
     {
       type: "tiles",
-      src: "img/horror/юра/тайл8.png",
+      src: "img/horror/yura/tile8.png",
       author: "Юра"
     },
     {
       type: "tiles",
-      src: "img/horror/юра/тайл9.png",
+      src: "img/horror/yura/tile9.png",
       author: "Юра"
     },
     {
       type: "tiles",
-      src: "img/horror/юра/тайл10.png",
+      src: "img/horror/yura/tile10.png",
       author: "Юра"
     },
     {
       type: "tiles",
-      src: "img/horror/юра/тайл11.png",
+      src: "img/horror/yura/tile11.png",
       author: "Юра"
     },
     {
       type: "tiles",
-      src: "img/horror/юра/тайл12.png",
+      src: "img/horror/yura/tile12.png",
       author: "Юра"
     },
     {
       type: "tiles",
-      src: "img/horror/маша/тайл1.png",
+      src: "img/horror/masha/tile1.png",
       author: "Маша"
     },
     {
       type: "tiles",
-      src: "img/horror/маша/тайл2.png",
+      src: "img/horror/masha/tile2.png",
       author: "Маша"
     },
     {
       type: "tiles",
-      src: "img/horror/маша/тайл3.png",
+      src: "img/horror/masha/tile3.png",
       author: "Маша"
     },
     {
       type: "tiles",
-      src: "img/horror/маша/тайл4.png",
+      src: "img/horror/masha/tile4.png",
       author: "Маша"
     },
     {
       type: "tiles",
-      src: "img/horror/маша/тайл5.png",
+      src: "img/horror/masha/tile5.png",
       author: "Маша"
     },
     {
       type: "tiles",
-      src: "img/horror/маша/тайл6.png",
+      src: "img/horror/masha/tile6.png",
       author: "Маша"
     },
     {
       type: "background",
-      src: "img/horror/юра/фон1.png",
+      src: "img/horror/yura/fon1.png",
       author: "Юра"
     },
     {
       type: "background",
-      src: "img/horror/юра/фон2.png",
+      src: "img/horror/yura/fon2.png",
       author: "Юра"
     },
     {
       type: "background",
-      src: "img/horror/егор/фон для босса.png",
+      src: "img/horror/egor/fon-dlya-bossa.png",
       author: "Егор"
     },
     {
       type: "background",
-      src: "img/horror/настя/фон.png",
+      src: "img/horror/nastya/fon.png",
       author: "Настя"
     },
     {
       type: "item",
-      src: "img/horror/егор/оружие.png",
+      src: "img/horror/egor/oruzhie.png",
       author: "Егор"
     },
    {
       type: "item",
-      src: "img/horror/юра/оружие.png",
+      src: "img/horror/yura/oruzhie.png",
       author: "Юра"
     },
     {
       type: "item",
-      src: "img/horror/настя/рюкзак.png",
+      src: "img/horror/nastya/ryukzak.png",
       author: "Настя"
     },
     {
       type: "item",
-      src: "img/horror/настя/палатка1.png",
+      src: "img/horror/nastya/palatka1.png",
       author: "Настя"
     },
     {
       type: "item",
-      src: "img/horror/настя/палатка2.png",
+      src: "img/horror/nastya/palatka2.png",
       author: "Настя"
     },
     {
       type: "hero",
-      src: "img/horror/настя/гг-полностью.png",
+      src: "img/horror/nastya/gg-polnostyu.png",
       author: "Настя"
     },
     {
       type: "hero",
-      src: "img/horror/настя/гг-голова.png",
+      src: "img/horror/nastya/gg-golova.png",
       author: "Настя"
     },
     {
       type: "enemy",
-      src: "img/horror/маша/злой-нпс.png",
+      src: "img/horror/masha/zloy-npc.png",
       author: "Маша"
     },
     {
       type: "enemy",
-      src: "img/horror/настя/нпс.png",
+      src: "img/horror/nastya/npc.png",
       author: "Настя"
     },
     {
       type: "ui",
-      src: "img/horror/настя/костер.gif",
+      src: "img/horror/nastya/koster.gif",
       author: "Настя"
     },
     {
       type: "ui",
-      src: "img/horror/настя/велоцираптор.gif",
+      src: "img/horror/nastya/velotsiraptor.gif",
       author: "Настя"
     },
     {
       type: "ui",
-      src: "img/horror/маша/меню.png",
+      src: "img/horror/masha/menu.png",
       author: "Маша"
     },
     {
       type: "ui",
-      src: "img/horror/юра/меню1.png",
+      src: "img/horror/yura/menu1.png",
       author: "Юра"
     },
     {
       type: "ui",
-      src: "img/horror/юра/меню2.png",
+      src: "img/horror/yura/menu2.png",
       author: "Юра"
     },
     {
       type: "ui",
-      src: "img/horror/настя/меню.png",
+      src: "img/horror/nastya/menu.png",
       author: "Настя"
     }
   ]
